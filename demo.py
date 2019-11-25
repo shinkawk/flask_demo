e794402c8d725dff96d4c83704f7c3f2c7786b49
shinkawk <shinkawk@gmail.com>
2019-11-26 02:45:27 +0900

    Added authentication hooray

--- demo.py
+++ demo.py
@@ -1,18 +1,72 @@
-from flask import Flask
+from flask import Flask, jsonify, session, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from authlib.integrations.flask_client import OAuth
+from functools import wraps
+
 import logging
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
+
+ma = Marshmallow(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootadmin@localhost:3306/testdb'
 db = SQLAlchemy(app)
 
 logging.basicConfig()
-logging.getLogger(__name__).setLevel(logging.DEBUG)
+logging.getLogger(__name__).setLevel(logging.INFO)
 
+oauth = OAuth(app)
 
-@app.route("/")
-def hello():
-    return str(User.query.all())
+auth0 = oauth.register(
+    'auth0',
+    client_id='7IfE7HlHEqAYfgBBzNhMrwiEoVl9kTcY',
+    client_secret='1PzZsweOre_cJPrrEC6k3IYyuW4ZvnsqSAahuL9ny51Q5s5r3WTUgJsKWdhaq-Uy',
+    api_base_url='https://dev-wkynz3v4.auth0.com',
+    access_token_url='https://dev-wkynz3v4.auth0.com/oauth/token',
+    authorize_url='https://dev-wkynz3v4.auth0.com/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
+
+@app.route('/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect("http://localhost:8080/")
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri='http://localhost:5000/callback')
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        print(session)
+        if 'profile' not in session:
+        # Redirect to Login page here
+            return redirect('/')
+        return f(*args, **kwargs)
+
+    return decorated
+
+@app.route("/api/users", methods=['GET'])
+@requires_auth
+def users():
+    users_schema = UserSchema(many=True)
+    return jsonify(users_schema.dump(User.query.all()))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +75,10 @@ class User(db.Model):
 
     def __repr__(self):
         return 'name:'+self.username+', email:'+str(self.email)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "email")
 
 if __name__ == "__main__":
     db.create_all()
